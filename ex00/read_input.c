07f24cbde112d90d5f8ba792d11b85904ccf9cea
Dariusz <dbrzozowski@poczta.onet.pl>
2026-05-17 06:45:01 +0200

    read_input added

---
+++ ex00/read_input.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+
+int read_input(char *input, char *top, char *bottom, char *left, char *right)
+{
+    int i;
+    int temp[16];
+
+    i = 0;
+    while (input != '\0')
+    {
+        temp[i] = input[2 * i] + '0';
+    }
+}
+
+int read_input_i(char *input, int *input_i)
+{
+    int i;
+    
+    i = 0;
+    while (i < 16)
+    {
+        input_i[i] = 0;
+    }
+
+    i = 0;
+    while (input != '\0')
+    {
+        input_i[i] = input[2 * i] + '0';
+    }
+
+    i = 0;
+    while (i < 16)
+    {
+        if (input_i[i] ==0)
+        {
+            return 0;
+        }
+    }
+
+    return (1);
+}
+void split_input_i( int input_i[16], int *ctop, int *cbottom, int *rleft, int *rright){
+    int i;
+
+    i = 0;
+    while (i < 4)
+    {
+        ctop[i] = input_i[i];
+        cbottom[i] = input_i[i+4];
+        rleft[i] = input_i[i+8];
+        rright[i] = input_i[i+12];
+    }
+}
