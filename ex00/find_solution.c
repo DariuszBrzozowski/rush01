e38a9f3c32d162c31a248bdd3f955c0329d967c1
Dariusz <dbrzozowski@poczta.onet.pl>
2026-05-17 10:05:53 +0200

    added cell availbility

--- ex00/find_solution.c
+++ ex00/find_solution.c
@@ -1,17 +1,5 @@
 
-void split_input_i(int input_i[16], int *ctop, int *cbottom, int *rleft, int *rright){
-    int i;
-
-    i = 0;
-    while (i < 4)
-    {
-        ctop[i] = input_i[i];
-        cbottom[i] = input_i[i+4];
-        rleft[i] = input_i[i+8];
-        rright[i] = input_i[i+12];
-        i++;
-    }
-}
+void split_input_i(int input_i[16], int *ctop, int *cbottom, int *rleft, int *rright);
 
 int check_top_input(int matrix[4][4], int ctop[4])
 {
@@ -154,6 +142,34 @@ int is_ok(int matrix[4][4], int input_i[16])
     return (1);
 
 }
+
+int cell_avail_for_num(int matrix[4][4], int row_index, int col_index, int num)
+{
+    int i;
+    
+    i = 0;
+    while (i < 4)
+    {
+        if (matrix[row_index][i] == num)
+        {
+            return (0);
+        }
+        i++;
+    }
+
+    i = 0;
+    while (i < 4)
+    {
+        if (matrix[i][col_index] == num)
+        {
+            return (0);
+        }
+        i++;
+    }
+
+    return (1);
+
+}
     
 
 int find_solution(int matrix[4][4], int input_i[16])
@@ -182,7 +198,7 @@ int find_solution(int matrix[4][4], int input_i[16])
 			matrix[row][col] = 0;
 			while (num <= 4 && counts[num] == 0)
 				num++;
-			if (num <= 4)
+			if (num <= 4 && cell_avail_for_num(matrix, row, col, num))
 			{
 				matrix[row][col] = num;
 				counts[num]--;
